0863c734fcf865bacdd3ddbf476c7bca236fb01a
Ben Purinton <ben.purinton@gmail.com>
2022-12-06 20:10:40 +0000

    user page just missing form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,15 @@ class UsersController < ApplicationController
     @list_of_users = User.all.order(username: :asc)
     render(template: "users_html/index")
   end
+
+  def show
+    @username = params.fetch("username")
+    @the_user = User.where(username: @username).first
+
+    if @the_user == nil
+      redirect_to("/404")
+    else
+      render(template: "users_html/show")
+    end
+  end
 end
